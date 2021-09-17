6bc633029c3ee362bc1f7b7e3693db35bd130b98
root <root@1bd838773b64>
2021-09-17 00:27:20 +0000

    Hello

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -13,9 +13,7 @@ public class RobotColorChooser {
 		//1. Create a new Robot
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
-		//4. Ask the user what color pen they would like the robot to draw with
-		//5. Use an if/else statement to set the pen color that the user requested
-        //6. If the user doesn't enter anything, choose a random color
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
@@ -23,31 +21,57 @@ public class RobotColorChooser {
 
 		rob.setSpeed(50);
 		rob.penDown();
+		
+		String answer = JOptionPane.showInputDialog("What color would you like?");
+		
+		if(answer.contains("red") )
+		rob.setPenColor(Color.red);{
+			
+		}
+		els
+		
+		//4. Ask the user what color pen they would like the robot to draw with
+		
+		//5. Use an if/else statement to set the pen color that the user requested
+		
+		//6. If the user doesn't enter anything, choose a random color
+		
+		
+		for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < 4; j++) {
 		rob.move(100);
 		rob.turn(90);
-		rob.move(100);
+		
+		}
 		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(0);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(0);
-		rob.move(100);
-		rob.turn(90);
-		rob.move(100);
-		rob.turn(90);
+		
+		}
+		
+rob.move(100);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		//rob.turn(0);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		//rob.turn(0);
+//		rob.move(100);
+//		rob.turn(90);
+//		rob.move(100);
+//		rob.turn(90);
 		
 		
 		
 		
 	}
-}
+
+	
+		
+	}
