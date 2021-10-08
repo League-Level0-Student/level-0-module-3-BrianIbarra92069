8ebac10c818d2df12b50d9d57169abfc25f8de47
root <root@20f0007e923a>
2021-10-08 00:29:49 +0000

    Nyaw

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,6 +3,8 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
@@ -14,6 +16,10 @@ public class CrazyCatLady {
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 		
+		String Answer =	JOptionPane.showInputDialog("How many cats do you have?");
+		
+		int number = Integer.parseInt();
+		double decimal = Double.parseDouble(Answer)
 		
 		
 		
