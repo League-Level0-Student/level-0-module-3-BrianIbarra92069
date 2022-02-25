741e290bf4400a2cefb4faeeb092d95ff8559824
root <root@7f6745b98627>
2022-02-25 02:26:28 +0000

    Hai

---
+++ src/_01_else_if/_4_are_you_happy/AreYouHappy.java
@@ -0,0 +1,54 @@
+package _01_else_if._4_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	string answer = JOptionPane.showInputDialog(null, "Are you happy");
+	
+	if(answer.contains("yes")) {
+	JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+	
+	}
+	
+	else
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
