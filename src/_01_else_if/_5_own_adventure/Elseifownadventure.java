acb83701dccf74fdf24a75bf5796e4c73e444402
root <root@7d7e355def26>
2022-03-25 01:26:52 +0000

    :D

---
+++ src/_01_else_if/_5_own_adventure/Elseifownadventure.java
@@ -0,0 +1,57 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class Elseifownadventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	JOptionPane.showConfirmDialog(null, "You live in a place where its haunted, and you want to explore around it,");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
