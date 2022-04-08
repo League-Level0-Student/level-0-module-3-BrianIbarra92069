a4da18d92ec5d91581dbc68eae40e3a00935df55
root <root@48a38b872d9d>
2022-04-08 01:28:36 +0000

    hai

--- src/_01_else_if/_5_own_adventure/Elseifownadventure.java
+++ src/_01_else_if/_5_own_adventure/Elseifownadventure.java
@@ -6,22 +6,30 @@ public class Elseifownadventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	JOptionPane.showConfirmDialog(null, "You live in a place where its haunted, and you want to explore around it,");
 	
 	
+	int choice = JOptionPane.showOptionDialog(null, "You live in a place where its haunted, and you want to explore around it, what would you use to light your way", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Flashlight", "Glowstick", "Phone", "Button4" }, null);
 	
+	if(choice == 0) {
+		JOptionPane.showMessageDialog(null, "You picked button 1");
 	
+	}
 	
+	if(choice == 1) {
+		JOptionPane.showMessageDialog(null, "You picked button 2");
 	
+	}
 	
+	if(choice == 2) {
+		JOptionPane.showMessageDialog(null, "You picked button 3");
 	
+	}
 	
+	if(choice == 3) {
+		JOptionPane.showMessageDialog(null, "You picked button 4");
 	
-	
-	
-	
-	
+	}
 	
 	
 	
