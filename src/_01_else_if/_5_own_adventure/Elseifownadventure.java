49c0221695585a48d52eaf7a78fe84a025b4ea7c
root <root@894afbc0661c>
2022-04-22 01:26:23 +0000

    Bai

--- src/_01_else_if/_5_own_adventure/Elseifownadventure.java
+++ src/_01_else_if/_5_own_adventure/Elseifownadventure.java
@@ -10,7 +10,9 @@ public class Elseifownadventure {
 	
 	int choice = JOptionPane.showOptionDialog(null, "You live in a place where its haunted, and you want to explore around it, what would you use to light your way", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
 			new String[] { "Flashlight", "Glowstick", "Phone", "Lamp" }, null);
+	
 	String lightsource = "";
+	
 	if(choice == 0) {
 		JOptionPane.showMessageDialog(null, "You picked Flashlight");
 		lightsource = "Flashlight";
@@ -31,8 +33,32 @@ public class Elseifownadventure {
 		lightsource = "Lamp";
 	}
 	
+	choice = JOptionPane.showOptionDialog(null, "Great, but in the house there are rooms that are odd", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Flashlight", "Glowstick", "Phone", "Lamp" }, null);
 	
+	String observationTool = "";
 	
+	if(choice == 0) {
+		JOptionPane.showMessageDialog(null, "You picked Flashlight");
+		observationTool = "Flashlight";
+	}
+	
+	if(choice == 1) {
+		JOptionPane.showMessageDialog(null, "You picked Glowstick");
+		observationTool = "Glowstick";
+	}
+	
+	if(choice == 2) {
+		JOptionPane.showMessageDialog(null, "You picked Phone");
+		observationTool = "Phone";
+	}
+	
+	if(choice == 3) {
+		JOptionPane.showMessageDialog(null, "You picked Lamp");
+		observationTool = "Lamp";
+	}
+	
+	// i want the person to also equip something that can help them look for odd things.
 	
 	
 	
