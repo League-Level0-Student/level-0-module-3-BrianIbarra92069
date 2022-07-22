0df773f74467442f1a062e59b733c0bd168f12e1
root <root@25e22a8d5dc5>
2022-07-22 01:24:21 +0000

    Hai

---
+++ src/_02_loop_variables/_1_years_alive/YearsAlive.java
@@ -0,0 +1,56 @@
+package _02_loop_variables._1_years_alive;
+
+public class YearsAlive {
+
+}
+
+
+	for( int years = )
+	
+	}
+	
+	
+	
+	
+	
+	private void Syso(int counter2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
