456dc47e923b2cc8f3d032d319b1b915ac0c23bb
root <root@34c79f1fac8a>
2022-09-02 01:28:52 +0000

    Hai

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -1,6 +1,8 @@
 
 package _02_loop_variables._3_robot_walk_of_fame;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
@@ -9,9 +11,20 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setSpeed(55);
+		rob.penDown();
+		rob.setX(500);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		String answer =JOptionPane.showInputDialog(null, "what color would you like");
+		
+		for(int i = 0; i < 5; i++) {
+		rob.move(89);
+		rob.turn(144);
+			
+		}	
+			
+			
+			
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
